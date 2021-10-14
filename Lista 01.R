7f2ef5434a49e2fbb9a8a0b3d4d405c4bb59d318
Matheus Brasil <matheusbrasil122@gmail.com>
2021-10-14 17:15:30 -0300

    medidas
    calculo de medidas de dipersão e documentação do script

--- Lista 01.R
+++ Lista 01.R
@@ -1,8 +1,8 @@
-'''
-01. Sejam os dados a seguir, já ordenados do menor para o 
-maior (rol) de 50 observações, em decibéis, de nível de
-ruído de tráfego em certo cruzamento.
-'''
+###############################################################################
+#Sejam os dados a seguir, já ordenados do menor para o maior (rol) de 50      #
+#observações, em decibéis, de nível de ruído  de tráfego em certo cruzamento. #
+#Pede-se:                                                                     #
+###############################################################################
 dados <- c(52.0, 55.9, 56.7, 59.4, 60.2, 
            61.0, 62.1, 63.8, 65.7, 67.9, 
            54.4, 55.9, 56.8, 59.4, 60.3, 
@@ -14,33 +14,58 @@ dados <- c(52.0, 55.9, 56.7, 59.4, 60.2,
            55.8, 56.4, 58.9, 60.0, 60.8, 
            62.0, 63.6, 64.9, 67.1, 77.1)
 
-#Pede-se:
 
-#a) Calcular média, moda e mediana para o rol
-
+################################################
+#(A) Calcular média, moda e mediana para o rol #
+################################################
 media <- mean(dados)
 media
 
-## para calcular a moda é necessário usar uma função própria
+#para calcular a moda é necessário usar uma função própria
 mode <- function(x) {
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
   }
-        #este método é melhor para séries grandes e e multimodal.
+
+#este método é melhor para séries grandes e e multimodal.
 moda <- subset(table(dados), table(dados) == max(table (dados)))
 moda
 
 mediana <- median(dados)
 mediana
+####################################################################################
+#B) Compare as 3 medidas do item (A) e faça a descrição sucinta dessas observações #
+####################################################################################
+amplitude <- range(dados) #amplitude total
+amplitude
+77.1 - 55.2
 
-#b) Compare as 3 medidas e faça a descrição sucinta dessas observações
+desvio_med <- sum(abs(dados-media)/length(dados)) #desvio médio
+desvio_med 
+
+IQR(dados, type=4) #amplitude interquartílica 
+
+var(dados) #variância
+
+sd(dados) #desvio padrão
+
+
 
 '''
-#Descreve a posição central, variabilidade, assimetria e curtose.
+Posição Central: O conjunto de dados apresentou média de 61.386 db, mediana de 
+60.9 e três modas que são os valores 55.9, 56.4 e 59.4.
+variabilidade: A amplitude total do cojunto é de 21.9. O desvio médio é de 3.76544.
+A amplitude interqualítica é de 7.3. A variância dos dados é de 23.20368 e o 
+desvio padrão apresentado foi de 4.81702.
+
 '''
 
-#c) calcule Q1, Q3 e C80.
-#argumento type = 3 é para selecionar o método de cálculo.
+############################
+#(C) calcule Q1, Q3 e C80. #
+############################
+
+#argumento type = 4 é para selecionar o método de cálculo.
+
 quartil1 <- quantile(dados, 0.25, type = 4)
 quartil1
 
@@ -50,26 +75,41 @@ quartil3
 percentil80 <- quantile(dados, 0.80, type = 4)
 percentil80
 
-#d) construir tabela de distribuição de frequências agrupadas em classes.
+############################################################################
+#(D) construir tabela de distribuição de frequências agrupadas em classes. #
+############################################################################
 
-library(dplyr)
+library(dplyr) #pacote necessário para criar tabela de freq
 
-ni <- table(cut(dados, breaks = seq(52, 80,by=4),right=FALSE)) # Frequencias por categorias
-tab_frequencia<- rbind(ni, p_fi = 100*prop.table(ni)) # Frequencias relativas em %
+ni <- table(cut(dados,                      #input
+                breaks = seq(52, 80,by=4),  #classes
+                right=FALSE))               #intervalo aberto na direita
 
-tab_frequencia <- as.data.frame(t(cbind(tab_frequencia, c(sum(tab_frequencia[1,]),sum(tab_frequencia[2,])))),row.names=c(colnames(tab_frequencia),"Total")) #Construcao da tabela
+tab_frequencia<- rbind(ni,                        #frequencias por intervalo
+                       p_fi = 100*prop.table(ni)) #Frequencias relativas
+
+tab_frequencia <- as.data.frame(t(cbind(tab_frequencia,     #Construcao da tabela
+                                c(sum(tab_frequencia[1,]),  #soma das freq. absolutas
+                                sum(tab_frequencia[2,])))), #soma das freq. relativas
+                                row.names=c(colnames(tab_frequencia),
+                                            "Total")) 
+
 tab_frequencia <- transform(tab_frequencia,p_fi=round(p_fi,digits=2))
 tab_frequencia
+#########################################################################
+#(E) calcule a média moda e mediana para os dados agrupados em classes. #
+#########################################################################
 
-#e) calcule a média moda e mediana para os dados agrupados em classes.
+#################################
+#(F) compare os itens (A) e (E) #
+#################################
 
-#f) compare os itens a) e e)
 
-#g) Comparando as três medidas de Ⓔ o que você pode dizer a respeito dessa distribuição.
+#G) Comparando as três medidas de Ⓔ o que você pode dizer a respeito dessa distribuição.
 
-#h) plote o histrograma e compare com g)
+#H) plote o histrograma e compare com g)
 
-#i) calcular todas as medidas de dispersão
+#I) calcular todas as medidas de dispersão
 
 
 
